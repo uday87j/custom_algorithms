8f551f8c3621c8191ed6f3ffc1c697d97b992875
Udaya Ranga <udaya.ranga@arm.com>
2016-02-26 16:49:38 +0000

    IntToType
    Change-Id: I93b92023d367e9c3c613eeff426f1487a6bf34bd

--- struct_size.h
+++ struct_size.h
@@ -2,6 +2,7 @@
 #define CA_STRUCT_SIZE_H_
 
 #include <iostream>
+#include <map>
 
 namespace ca {
 
@@ -17,6 +18,63 @@ namespace ca {
     };
 
     extern std::ostream& operator << (std::ostream& os, const ca_int_t& ci);
+
+
+    //----------------Register & Deduction of types----------------------------
+    
+#define INT 0
+    
+    template <int id> struct id_to_type_t;
+    template <>   struct id_to_type_t<INT> { typedef int this_type; };
+
+    template <typename T> struct type_to_id_t;
+    template <> struct type_to_id_t<int>    { static const int id = INT; };
+
+    struct type_holder_t    {
+
+    };
+
+    // Illegal code:
+    // template <typename T>
+    //     struct atype_t  {
+    //         typedef T this_type;
+
+    //         template <typename R>
+    //             void change_type(const R& r)    {
+    //                 typedef R atype_t<T>::this_type;
+    //             }
+    //     };
+
+
+    template <typename T>
+        struct any_type_t   {
+            typedef T this_type;
+        };
+
+    struct type_db_t    {
+            type_db_t(const type_db_t&)  = delete;
+            type_db_t& operator = (const type_db_t&)  = delete;
+
+            static type_db_t& get_instance()   {
+                static type_db_t db;
+                return db;
+            }
+
+            template <typename T>
+                int register_type() {
+                    static std::map<int, any_type_t<T> > type_map_;
+                    any_type_t<T> atype;
+                    type_map_[type_count] = atype;
+                    return type_count++;
+                }
+
+            
+        private:
+            type_db_t() {}
+            static int type_count;
+
+    };
+    //----------------End Register & Deduction of types----------------------------
 }
 
 #endif
--- test_struct_size.cpp
+++ test_struct_size.cpp
@@ -1,5 +1,6 @@
 #include "struct_size.h"
 #include <iostream>
+#include <typeinfo>
 
 using namespace std;
 
@@ -9,8 +10,11 @@ namespace ca  {
         return ci.print(os);            
     }
 
+    int type_db_t::type_count = 0;
+
     void test_struct_size() {
 
+        // Test int v/s int inside struct
         int local_int   = 32;
         cout << "\nSize of local_int: " << sizeof(local_int) << endl;
         cout << "\nlcoal_int: " << local_int << endl;
@@ -19,5 +23,16 @@ namespace ca  {
         ca_int.ivar = 32;
         cout << "\nSize of ca_int: " << sizeof(ca_int) << endl;
         cout << "\nca_int: " << ca_int << endl;
+        //---------------------------------
+        
+        // Test type_db_t
+        auto& db = type_db_t::get_instance();
+        auto int_id = db.register_type<int>();
+        //auto int_type_wrap = db.recover_type(int_id);
+        //cout << "\nType in int_type_wrap: " << typeid(int_type_wrap::this_type).name() << endl;
+        const auto iid = type_to_id_t<int>::id;
+        //auto itype = id_to_type_t<iid>::this_type;
+        cout << "\nType in int_type_wrap: " << typeid(id_to_type_t<iid>::this_type).name() << endl;
+        // --------------------------------
     }
 }
